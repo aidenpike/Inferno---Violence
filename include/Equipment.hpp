44981254035b032dfc1d5db3eca4c8da8f14fa4b
Aiden Pike <aidenpike113@gmail.com>
2024-03-18 17:23:55 -0400

    Make declarations

---
+++ include/Equipment.hpp
@@ -0,0 +1,41 @@
+#ifndef EQUIPMENT_HPP
+#define EQUIPMENT_HPP
+
+#include <iostream>
+#include <fstream>
+
+namespace inferno {
+    class Equipment {
+        public:
+            int return_line_count(std::string); //Returns amount of lines in specified file
+            int return_random_int(int, int); //Min, max
+            
+            std::string get_equipment(int, std::string, int); //Creates an object and returns an attribute based on a number provided, the specified file, and an int denoting the index of the attribute
+
+            class Armor {
+                public:
+                    Armor(std::string, int, int); //Name, Defense Added, Mana Boost
+
+                    std::string get_name();
+                    int get_defense();
+                private:
+                    std::string m_name;
+                    int m_defense;
+                    int m_mana_boost;
+            };
+            
+            class Weapon {
+                public:
+                    Weapon(std::string, int, int); //Name, Damage Added, Mana Use
+
+                    std::string get_name();
+                    int get_damage();
+                    int get_mana_use();
+                private:
+                    std::string m_name;
+                    int m_damage;
+                    int m_mana_use;
+            };
+    };
+}; //Namespace inferno
+#endif //EQUIPMENT_HPP
